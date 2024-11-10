80250245d0254fa2332df3613229602f99d27389
rachelTeacher <rachel@haravdotnet.co.il>
2024-11-10 11:52:26 +0200

    fix enum

--- src/app/types/enums.ts
+++ src/app/types/enums.ts
@@ -1,8 +1,8 @@
-export  enum UserTypes{
+export enum UserTypes {
     systemAdministrator = 1,
-    accompanied =2 ,
-    normalUser = 3 ,
+    accompanied = 2,
+    normalUser = 3,
     managerOfAttendants = 4,
     usesUnderEscort = 5,
-    presenceUser =6 ,
- }
+    presenceUser = 6
+}
